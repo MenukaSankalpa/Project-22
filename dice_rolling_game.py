0c3643e4a4d776581b1494b6559206ac1760987b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-27 09:04:13 +0530

    improve user input and when answer is y

---
+++ dice_rolling_game.py
@@ -0,0 +1,16 @@
+#-Loop
+#--ask: roll the game ?
+#--if user enter y
+#----generate two numbers 
+#----print them
+#--If user enter n
+#----print thank you msg
+#----terminate game
+#--else
+#----print invalid choice 
+
+choice = input('Roll the Dice? (y/n): ').lover()
+if choice == 'y':
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    print(f'({die1}, {die2})')
